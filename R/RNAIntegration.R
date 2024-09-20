de47ff43e832451ef45d073c5dd6a46cd42f36e2
Yen-Chung Chen <yenchung.chen@nyu.edu>
2024-09-20 00:11:17 -0400

    Correct AddGeneIntegrationMatrix

--- R/RNAIntegration.R
+++ R/RNAIntegration.R
@@ -415,7 +415,7 @@ addGeneIntegrationMatrix <- function(
       useMatrix = useMatrix,
       verbose = FALSE
     )
-    rownames(mat) <- geneDF[geneDF$name %in% genesUse, "name"]
+    rownames(mat) <- as.character(geneDF[geneDF$name %in% genesUse, "name"])
     .logThis(mat, paste0("GeneScoreMat-Block-",i), logFile=logFile)
 
     #Impute Matrix (its already scaled internally in ArrowFiles)
